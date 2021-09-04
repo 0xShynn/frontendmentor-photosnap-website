ff7063623a3d50462aa4e5f52652433a415d5824
Antonin Nhek <a.nhek@icloud.com>
2021-09-04 20:29:57 +0200

    Initial commit from Create Next App

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+import overrides from '../theme/index'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={overrides}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
