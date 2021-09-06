6b3adee52d6b6da3dbf0aac77394677233117946
Antonin Nhek <a.nhek@icloud.com>
2021-09-06 19:02:34 +0200

    added: graphql logics for story page

---
+++ src/pages/stories/[slug].js
@@ -0,0 +1,68 @@
+import { Box, Text } from '@chakra-ui/layout'
+import { GraphQLClient, gql } from 'graphql-request'
+
+const client = new GraphQLClient(process.env.GRAPHCMS_PROJECT_API)
+
+export const getStaticPaths = async () => {
+  const query = gql`
+    query Stories {
+      stories {
+        slug
+      }
+    }
+  `
+
+  const data = await client.request(query)
+
+  return {
+    paths: data.stories.map((story) => ({ params: { slug: story.slug } })),
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params.slug
+
+  const query = gql`
+    query Stories($slug: String!) {
+      story(where: { slug: $slug }) {
+        id
+        title
+        photo {
+          url
+        }
+        author {
+          name
+          avatar {
+            url
+          }
+        }
+      }
+    }
+  `
+
+  const data = await client.request(query, { slug })
+
+  if (!data.story) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      story: { ...data.story },
+      revalidate: 60 * 60,
+    },
+  }
+}
+
+export default function Story({ story }) {
+  console.log(story)
+  return (
+    <Box w="full" bg="red.100">
+      <Text>{story.title}</Text>
+      <Text>{story.author.name}</Text>
+    </Box>
+  )
+}
